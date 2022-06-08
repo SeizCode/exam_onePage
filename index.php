0c17437323d086f96ccb76c835db1ff6f214b152
k-vin <vinz.sosaity@gmail.com>
2022-06-08 14:51:46 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<nav>
+        <div class="profile">
+            <img src="img/profile.jpg" alt="profile photo">
+            <h1>Kévin Debois</h1>
+        </div>
+
+        <ul>
+            <li><a href="github.php" class="text-with">GitHub</li>
+            <li><a href="projets.php" class="text-with">Mes projets</a></li>
+        </ul>
+    </nav>
+
+
+    <section class="presentation-image">
+            <img src="img/R2.jfif" alt="dev_web image">
+            
+        </section>
+
+        <footer>
+        <div class="colonne">
+            <h3>Mes services</h3>
+            <p>Developpement de projet web</p>
+            <p>Assistance du service réseau</p>
+        </div>
+        
+        <div class="colonne">
+            <h3>Mon centre de formation</h3>
+            <a href="https://www.simplon-charleville.fr">simplon.co</a>
+            <a href="https://www.ardennes.cci.fr/">CCI Ardennes</a>
+        </div>
+
+        <div class="colonne">
+            <h3>Mes adresses</h3>
+            <p>Debois.kevin@orange.fr</p>
+        </div>
+    </footer>
+
+
+
+    
+</body>
+</html>
---
+++ projets.php
@@ -0,0 +1,195 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Projets</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 700px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+  </head>
+  <body>
+    
+<header>
+  <div class="collapse bg-dark" id="navbarHeader">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8 col-md-7 py-4">
+          <h4 class="text-white"></h4>
+            <p class="text-muted"></p>
+              </div>
+                <div class="col-sm-4 offset-md-1 py-4">
+              <h4 class=text-white>Retour</h4>
+            <ul class="list-unstyled">
+          <li><a href="index.php"class ="text-white">Accueil</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+  <div class="navbar navbar-dark bg-dark shadow-sm">
+    <div class="container">
+      <a href="#" class="navbar-brand d-flex align-items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></svg>
+          <strong>Mes projets</strong></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+  </div>
+</header>
+
+<main>
+
+<div class="album py-5 bg-light">
+  <div class="container">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+      <div class="col">
+        <div class="card shadow-sm">
+          <img src = "img/projectcycle.jpg" width="420px" height="225px">
+            <div class="card-body">
+              <p class="card-text"></p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <form>
+                  <a href="">
+                <input type="button" value="Projets">
+              </a>
+            </form>
+          </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="col">
+  <div class="card shadow-sm">
+    <img src = "img/projectcycle.jpg" width="420px" height="225px">
+      <div class="card-body">
+        <p class="card-text"></p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+              <form>
+                <a href="">
+              <input type="button" value="Projets">
+            </a>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="col">
+  <div class="card shadow-sm">
+    <img src = "img/projectcycle.jpg" width="420px" height="225px">
+      <div class="card-body">
+        <p class="card-text"></p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+              <form>
+                <a href="">
+              <input type="button" value="Projets">
+            </a>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="col">
+  <div class="card shadow-sm">
+    <img src = "img/projectcycle.jpg" width="420px" height="225px">
+      <div class="card-body">
+        <p class="card-text"></p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+              <form>
+                <a href="">
+              <input type="button" value="Projets">
+            </a>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="col">
+  <div class="card shadow-sm">
+    <img src = "img/projectcycle.jpg" width="420px" height="225px">
+      <div class="card-body">
+        <p class="card-text"></p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+              <form>
+                <a href="">
+              <input type="button" value="Projets">
+            </a>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="col">
+  <div class="card shadow-sm">
+    <img src = "img/projectcycle.jpg" width="420px" height="225px">
+      <div class="card-body">
+        <p class="card-text"></p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+              <form>
+                <a href="">
+              <input type="button" value="Projets">
+            </a>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</main>
+
+<footer class="text-muted py-5">
+  <div class="container">
+    <p class="float-end mb-1">
+      <a href="#">Retour en haut</a>
+    </p>
+    <p class="mb-1">L'exemple d'album est  &copy; Bootstrap, mais veuillez le télécharger et le personnaliser par vous-même !</p>
+    <p class="mb-0">Nouveau bootstrap? <a href="/">Visiter la page d'accueil</a> où lisez notre <a href="/docs/5.1/getting-started/introduction/">Guide de démarrage</a>.</p>
+  </div>
+</footer>
+
+</body>
+</html>
+
+    <script src="/docs/5.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
